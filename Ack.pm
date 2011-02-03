ada99983d820b2c61e68c50d63ed68e18ff472c0
Andy Lester <andy@petdance.com>
2011-02-02 19:04:32 -0600

    starting the building

---
+++ Ack.pm
@@ -0,0 +1,34 @@
+package App::Ack;
+
+use warnings;
+use strict;
+
+use File::Next 0.40;
+
+=head1 NAME
+
+App::Ack - A container for functions for the ack program
+
+=head1 VERSION
+
+Version 2.00a01
+
+=cut
+
+our $VERSION;
+our $COPYRIGHT;
+BEGIN {
+    $VERSION = '2.00a01';
+    $COPYRIGHT = 'Copyright 2005-2011 Andy Lester.';
+}
+
+
+=head1 SYNOPSIS
+
+If you want to know about the F<ack> program, see the F<ack> file itself.
+
+No user-serviceable parts inside.  F<ack> is all that should use this.
+
+=cut
+
+1;
