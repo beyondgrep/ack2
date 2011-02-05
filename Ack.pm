765bcb954ecce02523c81f8f4e4cabc4780b30c7
Andy Lester <andy@petdance.com>
2011-02-04 23:37:55 -0600

    bringing over functions from Ack.pm that I know I have to have

--- Ack.pm
+++ Ack.pm
@@ -17,7 +17,6 @@ Version 2.00a01
 
 our $VERSION;
 our $COPYRIGHT;
-
 BEGIN {
     $VERSION = '2.00a01';
     $COPYRIGHT = 'Copyright 2005-2011 Andy Lester.';
@@ -29,18 +28,77 @@ BEGIN {
     $fh = *STDOUT;
 }
 
+
+our %types;
+our %type_wanted;
+our %mappings;
+our %ignore_dirs;
+
+our $input_from_pipe;
+our $output_to_pipe;
+
+our $dir_sep_chars;
+our $is_cygwin;
+our $is_windows;
+
+use File::Spec ();
+use File::Glob ':glob';
+use Getopt::Long ();
+
+BEGIN {
+    # These have to be checked before any filehandle diddling.
+    $output_to_pipe  = not -t *STDOUT;
+    $input_from_pipe = -p STDIN;
+
+    $is_cygwin       = ($^O eq 'cygwin');
+    $is_windows      = ($^O =~ /MSWin32/);
+    $dir_sep_chars   = $is_windows ? quotemeta( '\\/' ) : quotemeta( File::Spec->catfile( '', '' ) );
+}
+
 =head1 SYNOPSIS
 
 If you want to know about the F<ack> program, see the F<ack> file itself.
 
 No user-serviceable parts inside.  F<ack> is all that should use this.
 
+=head1 FUNCTIONS
+
+=head2 read_ackrc
+
+Reads the contents of the .ackrc file and returns the arguments.
+
 =cut
 
-# print subs added in order to make it easy for a third party
-# module (such as App::Wack) to redefine the display methods
-# and show the results in a different way.
-sub print { print {$fh} @_ }
+sub read_ackrc {
+    my @files = ( $ENV{ACKRC} );
+    my @dirs =
+        $is_windows
+            ? ( $ENV{HOME}, $ENV{USERPROFILE} )
+            : ( '~', $ENV{HOME} );
+    for my $dir ( grep { defined } @dirs ) {
+        for my $file ( '.ackrc', '_ackrc' ) {
+            push( @files, bsd_glob( "$dir/$file", GLOB_TILDE ) );
+        }
+    }
+    for my $filename ( @files ) {
+        if ( defined $filename && -e $filename ) {
+            open( my $fh, '<', $filename ) or App::Ack::die( "$filename: $!\n" );
+            my @lines = grep { /./ && !/^\s*#/ } <$fh>;
+            chomp @lines;
+            close $fh or App::Ack::die( "$filename: $!\n" );
+
+            # get rid of leading and trailing whitespaces
+            for ( @lines ) {
+               s/^\s+//;
+               s/\s+$//;
+            }
+
+            return @lines;
+        }
+    }
+
+    return;
+}
 
 sub _get_thpppt {
     my $y = q{_   /|,\\'!.x',=(www)=,   U   };
@@ -54,4 +112,219 @@ sub _thpppt {
     exit 0;
 }
 
-1;
+=head2 show_help()
+
+Dumps the help page to the user.
+
+=cut
+
+sub show_help {
+    my $help_arg = shift || 0;
+
+#   return show_help_types() if $help_arg =~ /^types?/;
+
+    App::Ack::print( <<"END_OF_HELP" );
+Usage: ack [OPTION]... PATTERN [FILE]
+
+Search for PATTERN in each source file in the tree from cwd on down.
+If [FILES] is specified, then only those files/directories are checked.
+ack may also search STDIN, but only if no FILE are specified, or if
+one of FILES is "-".
+
+Default switches may be specified in ACK_OPTIONS environment variable or
+an .ackrc file. If you want no dependency on the environment, turn it
+off with --noenv.
+
+Example: ack -i select
+
+Searching:
+
+Search output:
+
+File presentation:
+
+File finding:
+
+File inclusion/exclusion:
+
+Miscellaneous:
+  --thpppt              Bill the Cat
+
+Exit status is 0 if match, 1 if no match.
+
+This is version $VERSION of ack.
+END_OF_HELP
+
+    return;
+ }
+
+
+=head2 get_version_statement
+
+Returns the version information for ack.
+
+=cut
+
+sub get_version_statement {
+    require Config;
+
+    my $copyright = get_copyright();
+    my $this_perl = $Config::Config{perlpath};
+    if ($^O ne 'VMS') {
+        my $ext = $Config::Config{_exe};
+        $this_perl .= $ext unless $this_perl =~ m/$ext$/i;
+    }
+    my $ver = sprintf( '%vd', $^V );
+
+    return <<"END_OF_VERSION";
+ack $VERSION
+Running under Perl $ver at $this_perl
+
+$copyright
+
+This program is free software.  You may modify or distribute it
+under the terms of the Artistic License v2.0.
+END_OF_VERSION
+}
+
+=head2 print_version_statement
+
+Prints the version information for ack.
+
+=cut
+
+sub print_version_statement {
+    App::Ack::print( get_version_statement() );
+
+    return;
+}
+
+=head2 get_copyright
+
+Return the copyright for ack.
+
+=cut
+
+sub get_copyright {
+    return $COPYRIGHT;
+}
+
+=head2 load_colors
+
+Set default colors, load Term::ANSIColor
+
+=cut
+
+sub load_colors {
+    eval 'use Term::ANSIColor ()';
+
+    $ENV{ACK_COLOR_MATCH}    ||= 'black on_yellow';
+    $ENV{ACK_COLOR_FILENAME} ||= 'bold green';
+    $ENV{ACK_COLOR_LINENO}   ||= 'bold yellow';
+
+    return;
+}
+
+
+# print subs added in order to make it easy for a third party
+# module (such as App::Wack) to redefine the display methods
+# and show the results in a different way.
+sub print                   { print {$fh} @_ }
+sub print_first_filename    { App::Ack::print( $_[0], "\n" ) }
+sub print_blank_line        { App::Ack::print( "\n" ) }
+sub print_separator         { App::Ack::print( "--\n" ) }
+sub print_filename          { App::Ack::print( $_[0], $_[1] ) }
+sub print_line_no           { App::Ack::print( $_[0], $_[1] ) }
+sub print_column_no         { App::Ack::print( $_[0], $_[1] ) }
+sub print_count {
+    my $filename = shift;
+    my $nmatches = shift;
+    my $ors = shift;
+    my $count = shift;
+    my $show_filename = shift;
+
+    if ($show_filename) {
+        App::Ack::print( $filename );
+        App::Ack::print( ':', $nmatches ) if $count;
+    }
+    else {
+        App::Ack::print( $nmatches ) if $count;
+    }
+    App::Ack::print( $ors );
+}
+
+sub print_count0 {
+    my $filename = shift;
+    my $ors = shift;
+    my $show_filename = shift;
+
+    if ($show_filename) {
+        App::Ack::print( $filename, ':0', $ors );
+    }
+    else {
+        App::Ack::print( '0', $ors );
+    }
+}
+
+sub set_up_pager {
+    my $command = shift;
+
+    return if App::Ack::output_to_pipe();
+
+    my $pager;
+    if ( not open( $pager, '|-', $command ) ) {
+        App::Ack::die( qq{Unable to pipe to pager "$command": $!} );
+    }
+    $fh = $pager;
+
+    return;
+}
+
+=head2 input_from_pipe()
+
+Returns true if ack's input is coming from a pipe.
+
+=cut
+
+sub input_from_pipe {
+    return $input_from_pipe;
+}
+
+
+=head2 output_to_pipe()
+
+Returns true if ack's input is coming from a pipe.
+
+=cut
+
+sub output_to_pipe {
+    return $output_to_pipe;
+}
+
+=head2 exit_from_ack
+
+Exit from the application with the correct exit code.
+
+Returns with 0 if a match was found, otherwise with 1. The number of matches is
+handed in as the only argument.
+
+=cut
+
+sub exit_from_ack {
+    my $nmatches = shift;
+
+    my $rc = $nmatches ? 0 : 1;
+    exit $rc;
+}
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2005-2010 Andy Lester.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the Artistic License v2.0.
+
+=cut
+
+1; # End of App::Ack
