552ae5fee22151609000c4692bd4f20980d5d4e2
Andy Lester <andy@petdance.com>
2011-02-04 22:43:31 -0600

    Add --thpppt support

--- Ack.pm
+++ Ack.pm
@@ -17,11 +17,17 @@ Version 2.00a01
 
 our $VERSION;
 our $COPYRIGHT;
+
 BEGIN {
     $VERSION = '2.00a01';
     $COPYRIGHT = 'Copyright 2005-2011 Andy Lester.';
 }
 
+our $fh;
+
+BEGIN {
+    $fh = *STDOUT;
+}
 
 =head1 SYNOPSIS
 
@@ -30,5 +36,22 @@ If you want to know about the F<ack> program, see the F<ack> file itself.
 No user-serviceable parts inside.  F<ack> is all that should use this.
 
 =cut
+
+# print subs added in order to make it easy for a third party
+# module (such as App::Wack) to redefine the display methods
+# and show the results in a different way.
+sub print { print {$fh} @_ }
+
+sub _get_thpppt {
+    my $y = q{_   /|,\\'!.x',=(www)=,   U   };
+    $y =~ tr/,x!w/\nOo_/;
+    return $y;
+}
+
+sub _thpppt {
+    my $y = _get_thpppt();
+    App::Ack::print( "$y ack $_[0]!\n" );
+    exit 0;
+}
 
 1;
