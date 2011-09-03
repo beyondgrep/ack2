9344f7bd4823ea7e380ed6315acbfbaae0214c30
Rob Hoelz <rob@hoelz.ro>
2011-09-03 00:11:21 -0500

    Add ConfigFinder implementation

---
+++ ConfigFinder.pm
@@ -0,0 +1,67 @@
+package App::Ack::ConfigFinder;
+
+=head1 App::Ack::ConfigFinder
+
+=cut
+
+use strict;
+use warnings;
+
+use Cwd ();
+use File::Spec;
+
+my $has_win32 = eval {
+    require Win32;
+};
+
+=head1 METHODS
+
+=head2 new
+
+Creates a new config finder.
+
+=cut
+
+sub new {
+    my ( $class ) = @_;
+
+    return bless {}, $class;
+}
+
+=head2 $finder->find_config_files
+
+Locates config files, and returns
+a list of them.
+
+=cut
+
+sub find_config_files {
+    my @config_files;
+
+    if($has_win32) {
+        no strict 'subs';
+        push @config_files, (
+            Win32::GetFolderPath(Win32::CSIDL_COMMON_APPDATA),
+            Win32::GetFolderPath(Win32::CSIDL_APPDATA),
+        );
+    } else {
+        push @config_files, '/etc/ackrc' if $^O ne 'MSWin32';
+        if(defined(my $home = $ENV{'HOME'})) {
+            push @config_files, File::Spec->catfile($home, '.ackrc');
+        }
+    }
+
+    my @dirs = File::Spec->splitdir(Cwd::getcwd());
+    while(@dirs) {
+        my $ackrc = File::Spec->catfile(@dirs, '.ackrc');
+        if(-f $ackrc) {
+            push @config_files, $ackrc;
+            last;
+        }
+        pop @dirs;
+    }
+
+    return @config_files;
+}
+
+1;
