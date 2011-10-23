58285ecd1c4424fcbba8a9afef064ec666df46a6
Rob Hoelz <rob@hoelz.ro>
2011-10-23 18:57:09 -0500

    Pass filter tests

--- Filter.pm
+++ Filter.pm
@@ -3,10 +3,24 @@ package App::Ack::Filter;
 use strict;
 use warnings;
 
+use Carp ();
+
+my %filter_types;
+
 sub create_filter {
     my ( undef, $type, @args ) = @_;
 
-    return 1;
+    if(my $package = $filter_types{$type}) {
+        return $package->new(@args);
+    } else {
+        Carp::croak "Unknown filter type '$type'";
+    }
+}
+
+sub register_filter {
+    my ( undef, $type, $package ) = @_;
+
+    $filter_types{$type} = $package;
 }
 
 1;
