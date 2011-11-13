e2e00215eed773a8dbf8fde0a24bcbb23b1b4408
Andy Lester <andy@petdance.com>
2011-11-13 17:36:50 -0600

    add a comment, and fix qr// quoting

--- FirstLineMatch.pm
+++ FirstLineMatch.pm
@@ -6,11 +6,16 @@ use base 'App::Ack::Filter';
 
 sub new {
     my ( $class, $re ) = @_;
-    $re =~ s!^/|/$!!g; # XXX validate?
-    $re = qr/$re/i;
+    $re =~ s{^/|/$}{}g; # XXX validate?
+    $re = qr{$re}i;
 
     return bless \$re, $class;
 }
+
+# XXX This test checks the first "line" of the file, but we need
+# it to be less piggy.  If it's something like a .min.js file, then
+# the "line" could be the entire file.  Instead, it should read the
+# first, say, 100 characters of the first line.
 
 sub filter {
     my ( $self, $resource ) = @_;
