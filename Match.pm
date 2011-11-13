d3ac58e1a4bf7b4f19a941a3217eac239a5bf205
Rob Hoelz <rob@hoelz.ro>
2011-11-13 13:34:49 -0600

    Add match filter implementation

---
+++ Match.pm
@@ -0,0 +1,31 @@
+package App::Ack::Filter::Match;
+
+use strict;
+use warnings;
+use base 'App::Ack::Filter';
+
+use File::Spec;
+
+sub new {
+    my ( $class, $re ) = @_;
+
+    $re =~ s!^/|/$!!g; # XXX validate?
+    $re = qr/$re/i;
+
+    return bless \$re, $class;
+}
+
+sub filter {
+    my ( $self, $resource ) = @_;
+
+    my $re                     = $$self;
+    my ( undef, undef, $base ) = File::Spec->splitpath($resource->name);
+
+    return $base =~ /$re/;
+}
+
+BEGIN {
+    App::Ack::Filter->register_filter(match => __PACKAGE__);
+}
+
+1;
