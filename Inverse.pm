8a2d65c7d7925ffbd8787ef0aba5260762adb4e4
Rob Hoelz <rob@hoelz.ro>
2011-11-28 21:56:51 -0600

    Add inverse filter implementation

---
+++ Inverse.pm
@@ -0,0 +1,30 @@
+package App::Ack::Filter::Inverse;
+
+use strict;
+use warnings;
+use base 'App::Ack::Filter';
+
+sub new {
+    my ( $class, $filter ) = @_;
+
+    return bless \$filter, $class;
+}
+
+sub filter {
+    my ( $self, $resource ) = @_;
+
+    my $filter = $$self;
+    return !$filter->filter( $resource );
+}
+
+sub invert {
+    my ( $self ) = @_;
+
+    return $$self;
+}
+
+sub is_inverted {
+    return 1;
+}
+
+1;
