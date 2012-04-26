bbd49a4963492868c914a0a22016f581cdbc2c62
Rob Hoelz <rob@hoelz.ro>
2012-04-26 23:39:43 +0200

    Add a developer script for summarized option coverage

---
+++ dev/display-option-coverage.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use lib 'blib/lib';
+
+use App::Ack::ConfigLoader ();
+
+my $arg_spec = App::Ack::ConfigLoader::get_arg_spec({});
+
+my $fh;
+open $fh, '<', 'opts.coverage' or die $!;
+
+while(<$fh>) {
+    chomp;
+
+    delete $arg_spec->{$_};
+}
+
+close $fh;
+
+if(keys %{$arg_spec}) {
+    print "The following options were not used in the test suite:\n\n";
+    print "  $_\n" foreach sort keys %{$arg_spec};
+} else {
+    print "All options were used in the test suite\n";
+}
