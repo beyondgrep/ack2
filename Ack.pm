7701e00da359844c3512bc1b8445130984cf3066
Andy Lester <andy@petdance.com>
2013-12-03 07:12:56 -0600

    Releasing 2.12

--- Ack.pm
+++ Ack.pm
@@ -9,14 +9,14 @@ App::Ack - A container for functions for the ack program
 
 =head1 VERSION
 
-Version 2.11_02
+Version 2.12
 
 =cut
 
 our $VERSION;
 our $COPYRIGHT;
 BEGIN {
-    $VERSION = '2.11_02';
+    $VERSION = '2.12';
     $COPYRIGHT = 'Copyright 2005-2013 Andy Lester.';
 }
 
