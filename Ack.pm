1085dc5feb50f3b85a2388f8aa199bd3ad93b0b9
Andy Lester <andy@petdance.com>
2013-12-03 08:18:42 -0600

    Getting ready for dev towards 2.14

--- Ack.pm
+++ Ack.pm
@@ -9,14 +9,14 @@ App::Ack - A container for functions for the ack program
 
 =head1 VERSION
 
-Version 2.12
+Version 2.13_01
 
 =cut
 
 our $VERSION;
 our $COPYRIGHT;
 BEGIN {
-    $VERSION = '2.12';
+    $VERSION = '2.13_01';
     $COPYRIGHT = 'Copyright 2005-2013 Andy Lester.';
 }
 
