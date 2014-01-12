dcb5bc8ca1cbbd230df6ef3fd58f8ce58babe220
Andy Lester <andy@petdance.com>
2014-01-12 00:42:25 -0600

    Added a check on the system() call that has been failing

--- t/Util.pm
+++ t/Util.pm
@@ -238,7 +238,8 @@ sub run_cmd {
             my $input_command = Win32::ShellQuote::quote_system_string(@{$input});
             $cmd = "$input_command | $cmd";
         }
-        system $cmd;
+        system( $cmd ) == 0
+            or die "system( $cmd ) failed: $?";
         close STDOUT;
         close STDERR;
         open(STDOUT, ">&SAVEOUT") or die "Can't restore STDOUT: $!";
