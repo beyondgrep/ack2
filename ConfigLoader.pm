fe8b3044b1f180f537b26ed3870e51f8f87e3480
Packy Anderson <Packy_Anderson@bmc.com>
2014-03-27 17:52:10 -0400

    Implement a -K|--no-known-types switch to turn off -k|--known-types.

--- ConfigLoader.pm
+++ ConfigLoader.pm
@@ -179,6 +179,10 @@ sub process_filetypes {
         push @{ $opt->{'filters'} }, @filters;
     };
 
+    $additional_specs{'K|no-known-types'} = sub {
+        @{ $opt->{'filters'} } = ();
+    };
+
     return \%additional_specs;
 }
 
