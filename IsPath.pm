07de134370187cd7c5a683b25049284ac37433e6
Rob Hoelz <rob@hoelz.ro>
2014-10-03 08:57:36 -0500

    Add IsPath filter
    For implementing --ignore-dir in terms of relative paths

---
+++ IsPath.pm
@@ -0,0 +1,44 @@
+package App::Ack::Filter::IsPath;
+
+use strict;
+use warnings;
+use base 'App::Ack::Filter';
+
+use App::Ack::Filter::IsGroup;
+
+sub new {
+    my ( $class, $filename ) = @_;
+
+    return bless {
+        filename => $filename,
+        #groupname => 'IsPathGroup',
+    }, $class;
+}
+
+sub create_group {
+    return App::Ack::Filter::IsPathGroup->new();
+}
+
+sub filter {
+    my ( $self, $resource ) = @_;
+
+    return $resource->name eq $self->{'filename'};
+}
+
+sub inspect {
+    my ( $self ) = @_;
+
+    my $filename = $self->{'filename'};
+
+    return ref($self) . " - $filename";
+}
+
+sub to_string {
+    my ( $self ) = @_;
+
+    my $filename = $self->{'filename'};
+
+    return $filename;
+}
+
+1;
