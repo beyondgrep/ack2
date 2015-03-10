a92ee844d97a9e4f98aedca8953ab9a3bc0ffdb6
Daniel Perrett <perrettdl@googlemail.com>
2015-03-10 09:06:23 +0000

    dev/timings.pl should exit and warn if cannot find parrot

--- dev/timings.pl
+++ dev/timings.pl
@@ -15,6 +15,8 @@ use Time::HiRes qw(gettimeofday tv_interval);
 
 my $SOURCE_DIR = File::Spec->catdir($ENV{HOME}, 'parrot');
 
+-d $SOURCE_DIR or die "Expecting to find parrot in $SOURCE_DIR - get it from github.com/parrot/parrot";
+
 sub grab_versions {
     my @acks = @_;
 
